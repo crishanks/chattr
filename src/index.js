c3ec8f00888e89da1c66800ca434be6a76998678
github username <crishanks12@gmail.com>
2019-09-09 16:11:22 -0600

    set up socketio with new http server

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 const path = require('path')
+const http = require('http')
 const express = require('express')
+const socketio = require('socket.io')
 
 const app = express()
+//Create http web server ourselves to pass in to socketio, since we can't do it when server is automatically configured with express
+const server = http.createServer(app)
+const io = socketio(server)
+
 const port = process.env.PORT || 3000
 const publicDirectoryPath = path.join(__dirname, '../public')
 app.use(express.static(publicDirectoryPath))
 
-app.listen(port, () => {
+io.on('connection', () => {
+    console.log('New WebSocket connection')
+})
+
+server.listen(port, () => {
     console.log(`Server is up on port ${port}`)
 })
