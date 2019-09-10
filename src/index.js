0f39e8fd23e7b32489f0e952b09bdd2112646543
github username <crishanks12@gmail.com>
2019-09-09 21:27:37 -0600

    basic form input and socket connection established to emit message to all connected users.

--- src/index.js
+++ src/index.js
@@ -12,8 +12,11 @@ const port = process.env.PORT || 3000
 const publicDirectoryPath = path.join(__dirname, '../public')
 app.use(express.static(publicDirectoryPath))
 
-io.on('connection', () => {
+io.on('connection', (socket) => {
     console.log('New WebSocket connection')
+    socket.on('sendMessage', (message) => {
+        io.emit('message', message)
+    })
 })
 
 server.listen(port, () => {
